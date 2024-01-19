eca1b517009c937ff061fa1bcba5462db88af9c8
Tim Burke-Lehmann <84626697+Bylxly@users.noreply.github.com>
2024-01-19 22:54:20 +0100

    Update uninstall_netcontrol.sh

--- uninstall_netcontrol.sh
+++ uninstall_netcontrol.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+SCRIPT_PATH="/usr/share/netcontrol"
+
 # Entfernen des symbolischen Links
 sudo rm -f /usr/bin/netcontrol
 
@@ -9,7 +11,7 @@ sudo rm -f /usr/share/man/man1/netcontrol.1 2>&1 /dev/null
 sudo mandb 2>&1 /dev/null
 
 # Entfernen des Crontab-Eintrags
-(crontab -l | grep -v "netcontrol.sh") | crontab -
+crontab -l | grep -v "${script_dir}/netcontrol.sh" | crontab -
 
 # Entfernen des Skripts
 script_dir="$(dirname "$0")"
@@ -18,7 +20,6 @@ sudo rm -f "${script_dir}/config.sh"
 sudo rm -f "${script_dir}/install_netcontrol.sh" 2> /dev/null
 
 # Entfernen des Dateien in share
-SCRIPT_PATH="/usr/share/netcontrol"
 rm -f "$SCRIPT_PATH/netcontrol.sh"
 rm -f "$SCRIPT_PATH/config.sh"
 rm -f "$SCRIPT_PATH/log.log"
