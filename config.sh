8229080ac5de65632a2e21d53fb0e09f79826f0b
Tim Burke-Lehmann <84626697+Bylxly@users.noreply.github.com>
2024-01-19 22:42:55 +0100

    Add files via upload

---
+++ config.sh
@@ -0,0 +1,11 @@
+# Anzahl der Ping Versuche
+count=1
+
+# Timeout nach wievielen sekunden
+timeout=2
+
+# Zeit zwischen Intervallen
+interval=3
+
+# Expertenmodus (genauer ping feedback)
+expert=true
